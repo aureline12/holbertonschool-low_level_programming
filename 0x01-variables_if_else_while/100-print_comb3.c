afd48dbc399ec5690035a3086d35c4f0cb36d7d8
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-04 23:55:11 -0500

    print combo 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the function main use printf to print numbers
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+	int a = 0;
+	int j = 49;
+
+	for (i = 48; i <= 56; ++i)
+	{
+		for (; j <= 57; ++j)
+		{
+
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		a++;
+		j = 49 + a;
+	}
+	putchar('\n');
+	return (0);
+}
