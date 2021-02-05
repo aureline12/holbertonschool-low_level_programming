552c999406105258689a08c18b68f01ac41f51f6
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-04 23:19:11 -0500

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nz = 48;
+
+	while (nz <= 57)
+	{
+		putchar(nz);
+		nz++;
+	}
+	putchar(10);
+	return (0);
+}
