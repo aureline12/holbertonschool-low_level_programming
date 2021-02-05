68858f5445c4b80794bd64baece19aa409fef228
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-05 00:05:18 -0500

    fix v3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,34 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - in this code you can view la combination de los first 900 numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
-
 {
-	int fd = 48;
-	int ld;
+	int c;
+	int d = 49;
+	int u = 50;
 
-	while (fd <= 57)
+	for (c = 48; c <= 55; ++c)
 	{
-		ld = 48;
-
-			while (ld <= 57)
+		for (d = c + 1; d <= 56; ++d)
+		{
+			for (u = 50; u <= 57; ++u)
 			{
-				putchar(fd);
-				putchar(ld);
-				if (fd != 57 || ld != 57)
+				putchar(c);
+				putchar(d);
+				putchar(u);
+				if (c != 55 || d != 56 || u != 57)
 				{
 					putchar(44);
 					putchar(32);
 				}
-				ld++;
 			}
-		fd++;
+		}
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
