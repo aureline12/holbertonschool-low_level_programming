c89f25f151410229fe197a3b004b53646efca3ea
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-09 13:35:00 -0500

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* add - print the result of the sum
+* @a: the firts parameter
+* @b: the second number
+* Return: Always 0.
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
