48196069ed31f1b6956b0f95d6ec14c34c4cc5d3
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-11 12:03:30 -0500

    headers file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _isdigit - This function verify if c is digit
+* @c: the parameter char to comparate.
+* Return: 1 or 0.
+*/
+
+int _isdigit(int c)
+{
+	if (c == '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,24 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/* t0. _isupper - this prototype verify if is upper. */
+int _isupper(int c);
+/* t1. _isdigit - */
+int _isdigit(int c);
+/* t2. mul - */
+int mul(int a, int b);
+/* t3. print_numbers - */
+void print_numbers(void);
+/* t4. print_most_numbers - */
+void print_most_numbers(void);
+/* t5. more_numbers - */
+void more_numbers(void);
+/* t6. print_line - */
+void print_line(int n);
+/* t7. print_diagonal - */
+void print_diagonal(int n);
+/* t8. print_square - */
+void print_square(int size);
+/* t10. print_triangle -*/
+void print_triangle(int size);
+
+#endif /* HOLBERTON_H */
