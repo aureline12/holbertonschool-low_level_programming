1d7f152c9f7c13529dc8d7dbd44d9bbe50de60dd
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-11 11:17:35 -0500

    tasks 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _isupper - This function verify if is upper.
+* @c: the parameter char to comparate.
+* Return: 1 or 0.
+*/
+
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
