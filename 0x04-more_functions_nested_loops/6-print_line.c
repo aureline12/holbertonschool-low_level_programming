f5f12107e0a40ece1a35c624f6e6f415227f2e2e
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-11 16:55:58 -0500

    test code

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_line - this function print the line
+ * @n: the parameter to print the line
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('_');
+		print_line(n - 1);
+	}
+}
