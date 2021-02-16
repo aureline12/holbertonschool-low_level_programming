816e6cba367d440df11b10364483e44db2b6162d
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-15 20:14:43 -0500

    holberton file

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,21 @@
+/* This file constains all the prototypes of the projects */
+/* t0. reset_to_98 - */
+void reset_to_98(int *n);
+/* t1. swap_int - */
+void swap_int(int *a, int *b);
+/* t2.  _strlen - */
+int _strlen(char *s);
+/* t3. _puts - */
+void _puts(char *str);
+/* t4. print_rev - */
+void print_rev(char *s);
+/* t5. rev_string - */
+void rev_string(char *s);
+/* t6. puts2 -*/
+void puts2(char *str);
+/* t7. puts_half - */
+void puts_half(char *str);
+/* t8. print_array - */
+void print_array(int *a, int n);
+/* t9. _strcpy - */
+char *_strcpy(char *dest, char *src);
