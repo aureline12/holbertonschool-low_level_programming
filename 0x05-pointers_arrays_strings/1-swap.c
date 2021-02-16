be79d4c2e305cc98566be6f3fa6946a468a91189
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-15 22:44:18 -0500

    swap me

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * swap_int -
+ * @a: this is a parameter to change
+ * @b: this is the another parameter to change
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
