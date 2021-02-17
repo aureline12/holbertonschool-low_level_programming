c1c9e0cbb9c28f8c620809cf594d4786a0ed7f26
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-17 10:55:54 -0500

    add new loop while

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,9 +6,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = 0;
+	while (*(str + len) != 0)
+	{
+		len++;
+	}
 
-	while (str[i] != '\0')
+	while (i <= len)
 	{
 		_putchar(str[i]);
 		i += 2;
