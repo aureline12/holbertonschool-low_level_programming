a86882d2dcacfe83f9a28ee16957208848de8340
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-16 23:09:26 -0500

    add puts v2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts2 - this function print only the even numbers.
+ * @str: is the principal parameter.
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != 0)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			_putchar(0);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
