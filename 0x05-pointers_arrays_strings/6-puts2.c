44734bdc383d945e3c5d1af8e7a388793eb9adba
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-17 11:47:55 -0500

    add if again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != 0)
 	{
-		_putchar(str[i]);
-		i += 2;
+		if ((str[i] % 2) == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
