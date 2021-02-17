9af75e0a44dfe557f9fd0a212636622bd133391b
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-17 15:50:54 -0500

    change conditional

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,13 @@ void puts_half(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 	{
 	}
+
 	if ((len  % 2) == 0)
 		m = (len / 2);
 	else
 		m = (len - 1) / 2;
 
-	for (i = m; str[i] != '\0'; i++)
+	for (i = m; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
