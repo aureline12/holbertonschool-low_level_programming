ec0cb78772a81bc1d109d27506930989ff093856
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-18 11:29:36 -0500

    Initial files

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,23 @@
+/* This file constains all the prototypes of the projects */
+
+/* t0. t_strcat -  */
+char *_strcat(char *dest, char *src);
+/* t1. _strncat */
+char *_strncat(char *dest, char *src, int n);
+/* t2. _strncpy - */
+char *_strncpy(char *dest, char *src, int n);
+/* t3. _strcmp  -*/
+int _strcmp(char *s1, char *s2);
+/* t4. reverse_array - */
+void reverse_array(int *a, int n);
+/* t5. string_toupper - */
+char *string_toupper(char *);
+/* t6. cap_string - */
+char *cap_string(char *);
+/* t7. leet */
+char *leet(char *);
+/* t8. rot13 */
+char *rot13(char *);
+
+/* _putchar function. */
+int _putchar(char c);
