ae647ff0f3be15e52e08214efaa742709e7af64e
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-18 18:56:39 -0500

    0v2 strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,12 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-
 	int r = *s1 - *s2;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0' && r == 0)
 	{
-		return (r);
+		s1++;
+		s2++;
+		r = *s1 - *s2;
 	}
-	return (0);
+	return (r);
 }
