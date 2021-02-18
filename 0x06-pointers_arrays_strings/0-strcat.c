fbfc940913ce6fd4ee88079c1b370dd7be9254d1
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-18 16:25:12 -0500

    update comments

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,20 @@
 #include "holberton.h"
 /**
- * _strcat - check the code for Holberton School students.
+ * len - duplicate the pinter..
  *	@dest: pointer od destination.
- *	@src: pointer of src.
  * Return: Always 0.
  */
 int len(char *dest)
 {
 	return ((*dest != '\0') ? 1 + len(dest + 1) : 0);
 }
+/**
+ * _strcat - duplicate the pinter..
+ *	@dest: pointer od destination.
+ *	@src: pointer of src.
+ * Return: Always 0.
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	char *d_itter = dest + len(dest) - 1;
