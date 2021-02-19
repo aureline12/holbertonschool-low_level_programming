86a9f16401f3cfa05f783116981534979fa37796
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-18 22:49:46 -0500

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strncpy - this fuction copy the pointer to variable.
+ * @n: is the pointer is the origin.
+ * @dest: is the pointer is the destiny.
+ * @src: is the pointer is the destiny.
+ * Return: d.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
