3745eaaaf543f45acc27b74366bacd1d08c93dbc
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-20 16:18:21 -0500

    0x06 Firts Try Checks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,34 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * c_2_upper - fuction capitalize the strings.
+ * @letter: is the string to capitalize.
  */
+void c_2_upper(char *letter)
+{
+	if (*letter >= 'a' && *letter <= 'z')
+		*letter -= 32;
+}
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @str: is the string to capitalize.
+ * Return: str.
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char separators[] = " ,;.!?(){}\"\t\n";
+
+	for (i = 0; str[i] != '\0'; i++)
+		for (j = 0; separators[j] != '\0'; j++)
+			if (i == 0)
+			{
+				c_2_upper(&str[i]);
+				break;
+			}
+			else if (str[i - 1] == separators[j])
+			{
+				c_2_upper(&str[i]);
+				break;
+			}
+	return (str);
+}
