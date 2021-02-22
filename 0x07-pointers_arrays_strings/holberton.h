81333e9f04cdfe97acfae3d7a528c84a89ebee30
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-22 12:31:56 -0500

    v0 holberton.h

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+/* This file contains all the prototypes of the projects */
+
+/* t0. _memset - function that fills memory with a constant byte. */
+char *_memset(char *s, char b, unsigned int n);
+/* t1. _memcpy - function that copies memory area.*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+/* t2. _strchr - function that locates a character in a string. */
+char *_strchr(char *s, char c);
