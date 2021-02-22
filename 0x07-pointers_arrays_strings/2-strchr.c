e20a2e122c0ce37db19f2cc5beb381ced50644a4
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-22 13:22:20 -0500

    fix add return(0)

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,6 @@
  */
 char *_strchr(char *s, char c)
 {
-
 	int i;
 
 	for (i = 0; *(s + i) != '\0' ; i++)
@@ -18,4 +17,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
+	return (0);
 }
