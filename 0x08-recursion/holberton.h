982717c7a2dfe11e7f888e725043bff5ab521994
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-24 17:54:33 -0500

    add holberton.h

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,24 @@
+/* This file contains the prototypes necessary for the development of the project */
+
+/* 0. function that prints a string, followed by a new line. */
+void _puts_recursion(char *s);
+/* 1. void _print_rev_recursion(char *s); */
+void _print_rev_recursion(char *s);
+/* 2. function that returns the length of a string. */
+void _puts_recursion(char *s);
+/* 3. function that returns the factorial of a given number. */
+int factorial(int n);
+/* 4. function that returns the value of x raised to the power of y. */
+int _pow_recursion(int x, int y);
+/* 5. function that returns the natural square root of a number. */
+int _sqrt_recursion(int n);
+/* 6. function that returns 1 if the input integer is a prime number, otherwise return 0. */
+int is_prime_number(int n);
+/* 7. function that returns 1 if a string is a palindrome and 0 if not. */
+int is_palindrome(char *s);
+/* 8. function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0. */
+int wildcmp(char *s1, char *s2);
+/* 9.(Advanced) function that prints a string, followed by a new line. */
+void _puts_recursion(char *s);
+/* extra - _putchar - writes the character c to stdout */
+int _putchar(char c);
