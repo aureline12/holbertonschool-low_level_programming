39a369b0f76e79c787b188235370917e627f3bc4
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-24 19:43:44 -0500

    fix descrip for function

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string, followed by a new line
+ * @s: char contains the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+		_putchar('\n');
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
