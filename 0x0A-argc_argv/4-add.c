c02925ed43e02d9c3c4b03922fe6fccc1fed4408
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-01 17:04:45 -0500

    add 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print product of multiplication of two arguments
+ * @argc: counter of arguments
+ * @argv: arguments
+ * Return: (Success).
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 3 && argc > 97 && argc > 122)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	/* else if (argc > 97 && argc > 122)
+	{
+		printf("Error\n");
+		return (1);
+	} */
+	else
+	{
+		printf("%d\n", atoi(*(argv + 1)) + atoi(*(argv + 2)));
+		return (0);
+	}
+}
