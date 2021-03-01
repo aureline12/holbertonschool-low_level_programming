f0ad2cc47cd7edf452ebe18a1d1cf1a108d5ed48
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-28 20:07:04 -0500

    fix and refactor code

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+* main - is the main function
+* @argc: the number of command line arguments
+* @argv: array of size argc
+* Return: Always 0.
+*/
+
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	while(argc--)
-			printf("%s\n", *argv++);
+	printf("%d\n", argc - 1);
 	return (0);
 }
