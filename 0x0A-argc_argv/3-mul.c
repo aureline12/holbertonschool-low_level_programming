43da3987b5c876d3403114664c05dd434806081b
Sergioarg <sergio_ramos53@yahoo.com>
2021-02-28 20:39:47 -0500

    test 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - print product of multiplication of two arguments
  * @argc: counter of arguments
  * @argv: arguments
- * Return: (Success) : 1
+ * Return: (Success)
  */
 
 int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", atoi(*(argv + 1)) * atoi(*(argv + 2)));
 		return (0);
 	}
-	printf("Error\n");
-	return (1);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	
 }
