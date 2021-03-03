e91df614dd3bd4fbd141f8345f4fc2495ac9d89f
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-02 23:42:12 -0500

    add conditional

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,20 +7,17 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, k, l;
+	unsigned int i = 0, j = 0, k, l;
 	int size;
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 
-	i = 0;
 	while (*(s1 + i) != '\0')
 		i++;
-	j = 0;
 	while (*(s2 + j) != '\0')
 		j++;
 	size = i + j;
