396085cbcbca278d09fbac7213a21ccdecc55cd7
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-02 23:25:46 -0500

    refactor add new vars for loops

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * str_concat - function that concatenates two strings.
  * @s1: firts tring to concatenate
@@ -8,7 +7,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	unsigned int i, j, i2, j2;
 	char *sum_size;
 
 	if (s1 == NULL || s2 == NULL)
@@ -22,16 +21,16 @@ char *str_concat(char *s1, char *s2)
 
 	for (j = 0; s2[j] != '\0'; j++) /* get size of s2 */
 	{}
+/* --------------------------------------------------------- */
+	sum_size = malloc(((i + j + 1) * sizeof(char))); /* get size of s1 and s2 */
 
-	sum_size = malloc((i + j * sizeof(char) + 1)); /* get size of s1 and s2 */
+	for (i2 = 0; *(s1 + i2) != 0; i2++) /* add values of sum_size to s1*/
+		sum_size[i2] = *(s1 + i2);
 
-	for (i = 0; *(s1 + i) != 0; i++) /* add values of sum_size to s1*/
-		sum_size[i] = *(s1 + i);
-
-	for (j = 0; *(s2 + j) != 0; j++) /* add values of s1 to s2 */
+	for (j2 = 0; *(s2 + j2) != 0; j2++) /* add values of s1 to s2 */
 	{
-		sum_size[i]  = *(s2 + j);
-		i++;
+		sum_size[i2]  = *(s2 + j2);
+		i2++;
 	}
 	return (sum_size);
 }
