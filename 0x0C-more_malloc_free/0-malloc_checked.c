4cbd7d0a992b9d6577e46834282de58fe411ec14
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-04 14:16:39 -0500

    test new code

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,12 +8,9 @@ void *malloc_checked(unsigned int b)
 {
 	void *pointer;
 
-	pointer = malloc(b); /* We genve the Pointer Malloc  */
+	pointer = malloc(b);
 
 	if (pointer == NULL)
-	{
 		exit(98);
-	}
 	return (pointer);
-
 }
