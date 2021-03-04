3f23a9583da07b79832793293ed9e7a8b4a3280a
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-04 13:55:49 -0500

    add 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,7 +8,7 @@ void *malloc_checked(unsigned int b)
 {
 	void *pointer;
 
-	pointer = malloc(sizeof(int *) * b); /* We genve the Pointer Malloc  */
+	pointer = malloc(b); /* We genve the Pointer Malloc  */
 
 	if (pointer == NULL)
 	{
