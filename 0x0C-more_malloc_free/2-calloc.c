363cb3b58be65172ab45539debf27b06ca2bdb54
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-04 12:23:59 -0500

    add 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* _calloc - Function that allocates memory using malloc.
+* @nmemb: is the each
+* @size: is the size to find
+* Return: s.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *m_size;
+	/* error if is null */
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	/* malloc - reserve memory space */
+
+	m_size = malloc(size * sizeof(char));
+
+	/* possible error */
+	if (m_size == NULL)
+	{
+		return (NULL);
+	}
+
+	/* we go through the arrangement of malloc */
+	for (i = 0; i < size; i++)
+	{
+		*(m_size + i) = nmemb;
+	}
+	return (m_size);
+}
