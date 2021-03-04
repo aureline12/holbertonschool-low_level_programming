745956c792cf5c08c4da6239e25c6ef202634b18
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-04 13:54:34 -0500

    add 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* malloc_checked - Function that allocates memory using malloc.
+* @b:
+*
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(sizeof(int *) * b); /* We genve the Pointer Malloc  */
+
+	if (pointer == NULL)
+	{
+		exit (98);
+	}
+	else
+	{
+		return (pointer);
+	}
+
+}
