1a5851c42a0e369a919c97b391a5fc97a446bf6a
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-04 20:57:15 -0500

    fix refactor

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,11 +3,11 @@
 * array_range - Function that creates an array of integers.
 * @min: is the min
 * @max: is the mix
-* Return: s.
+* Return: array_range.
 */
 int *array_range(int min, int max)
 {
-	int i, length = (max - min) + 1; /* Here we drowned the amount to find */
+	int i, length = (max - min) + 1; /*Here we drowned the amount to find*/
 	int *array_range;
 
 	if (min > max)
