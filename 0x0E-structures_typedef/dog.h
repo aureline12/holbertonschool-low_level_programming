f80784ed93d8882b8361988bd48fd10da6909df4
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-08 19:18:54 -0500

    update add new prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: the age of the dog
  * @owner: the owner of the dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
+
