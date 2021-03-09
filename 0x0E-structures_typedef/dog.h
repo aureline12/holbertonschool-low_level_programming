1c70566d8dda9b35dbadbba41b76242288b4bc58
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-08 23:51:15 -0500

    add new prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,11 +12,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/* typedef dog_t of struct dog */
 typedef struct dog dog_t;
 /* Prototype 1 */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /* Prototype 2 */
 void print_dog(struct dog *d);
+/* Prototype 3 */
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
-
