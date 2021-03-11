493efed842782c892ee6be857d6430ac9f6f08ae
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-11 00:19:54 -0500

    change to null

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	}
 
 	if ((argv[2][0] == '/' || argv[2][0] == '%')
-		&& argv[2][1] == '\n' && argv[3][0] == '0')
+		&& argv[2][1] == '\0' && argv[3][0] == '0')
 	{
 		printf("Error\n");
 		exit(100);
