62b8f6649721acacbb7a4d2fee4bc0dc1ed9e9c6
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-11 20:10:00 -0500

    refactor variables

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,12 +10,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *letters;
 	unsigned int i;
 
-	if (separator == NULL)
-		separator = "";
-
 	va_list args;
 
 	va_start(args, n);
+
+	if (separator == NULL)
+		separator = "";
+
 
 	for (i = 0; i < n; i++)
 	{
