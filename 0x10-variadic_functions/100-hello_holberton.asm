d64eeb506573d0ea156146a663995343e4ad9fb3
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-11 22:21:20 -0500

    change to 64bytes syntax

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,16 @@
 section	.text
-   global _start     ;must be declared for linker (ld)
+   global main     ;must be declared for linker (ld)
 
-_start:	            ;tells linker entry point
-   mov	edx,len     ;message length
-   mov	ecx,msg     ;message to write
-   mov	ebx,1       ;file descriptor (stdout)
-   mov	eax,4       ;system call number (sys_write)
+main:	            ;tells linker entry point
+   mov	rdx,len     ;message length
+   mov	rcx,msg     ;message to write
+   mov	rbx,1       ;file descriptor (stdout)
+   mov	rax,4       ;system call number (sys_write)
    int	0x80        ;call kernel
 
-   mov	eax,1       ;system call number (sys_exit)
+   mov	rax,1       ;system call number (sys_exit)
    int	0x80        ;call kernel
 
-section	.data
+; section	.data
 msg db 'Hello, Holberton', 0xa  ;string to be printed
 len equ $ - msg     ;length of the string
