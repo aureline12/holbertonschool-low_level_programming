f916cfa0cca0e6d5ca981342fcfdb9e82138dcc6
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-18 22:31:42 -0500

    add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h:
+ */
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+
+	if (h->str)
+		printf("[%u] %s\n", h->len, h->str);
+	else
+		printf("[0] (nil)\n");
+
+	return (1 + print_list(h->next));
+
+}
