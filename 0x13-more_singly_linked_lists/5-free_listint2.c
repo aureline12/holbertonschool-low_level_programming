a8ef683ab1ef581e5931a1b3b28e76c2acc6cdb8
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-23 23:28:58 -0500

    Refactor and test other code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,18 +7,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *temp2;
 
-	if (*head == NULL)
+	if (!head)
 		return;
 
 	temp = *head;
 
-	while (temp)
+	while (*head)
 	{
-		temp2 = temp->next;
-		free(temp);
-		temp = temp2;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
 }
