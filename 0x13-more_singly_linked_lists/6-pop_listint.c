5b0f3466afc1c5faf615e2195ca1d4f7cde8064c
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-24 10:03:43 -0500

    Diferent if

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int num_values = 0;
 
-	if (head)
+	if (!head)
 		return (0);
 	/* Save value so you do not miss. */
 	num_values = (*head)->n;
