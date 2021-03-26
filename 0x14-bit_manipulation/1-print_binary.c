a139bd1aafa1c53df3b144ae424f51663b8e804c
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-26 12:04:32 -0500

    Add 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number.
+ * @n: Binary to print
+ * Return: Result number.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int tmp;
+	int i;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (tmp = n, i = 0; (tmp >>= 1); i++)
+	{}
+
+	for (; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
