6b811a3dcbe93e7c5b6604c2d8b29627368cd5ab
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-29 22:40:49 -0500

    Add initial files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads a text file and prints.
+ * @filename: is the name of file.
+ * @letters: number of letters it should read and print.
+ * Return: Nomber of letters
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	char *buffer;
+	ssize_t num_letters;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	file = open(filename, O_RDWR); /* Read and Write the file.*/
+
+	if (file == -1) /* Edge case  */
+		return (0);
+
+	num_letters = read(file, buffer, letters); /* Read buffer */
+	num_letters = write(STDOUT_FILENO, buffer, num_letters);/* Write buffer */
+	free(buffer);
+	close(file);
+	return (num_letters);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,9 @@
+# :file_folder: - Foldername
+
+_Insert description._
+
+### :memo: - Files
+
+#|Directory|Description
+---|---|---
+0|[README.md](./README.md)| Contains the description of the project.
