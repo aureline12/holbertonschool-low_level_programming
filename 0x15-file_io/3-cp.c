90d3bb545b3e07837a996e913055c5009fa3f060
Sergioarg <sergio_ramos53@yahoo.com>
2021-03-30 15:59:03 -0500

    Fix output conditional

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <unistd.h>
+#define BUFFER_SIZE 1024
 /**
  * main - Take a file and copy it to another file.
  * @argc: arguments to get the files
  * @argv: arguments to copy.
  * Return: Always 0
  */
-
 int main(int argc, char const *argv[])
 {
 	int file_from = 0, file_to = 0, read_file = 0, write_file = 0;
 	char bufffer[1024];
 
-	if (argc != 3)
+	if (argc != 3) /* Se valida si genera error todo lo diferente a 3 */
 	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
@@ -26,10 +26,10 @@ int main(int argc, char const *argv[])
 	/* Read, Create and Trunc the file whit -rw-rw-r-- */
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-	{	dprintf(STDERR_FILENO, "rror: Can't write to %s\n", argv[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	read_file = read(file_from, bufffer, 1024);
+	read_file = read(file_from, bufffer, BUFFER_SIZE);
 	if (read_file == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
