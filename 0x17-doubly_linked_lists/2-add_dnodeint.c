6afb693425f67bc4e8032e2a443e66b51bd9127f
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-02 00:59:19 -0500

    (Doc) Add comments

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,27 +7,27 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node; /* Create var struct */
 
-	if (head == NULL)
+	if (head == NULL) /* Validate if the firts pointer is null */
 		return (0);
-
+	/* Asignate space  in memory whit malloc */
 	new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
+	if (new_node == NULL) /* Validate if new node is null */
 		return (0);
 
-	new_node->n = n;
+	new_node->n = n; /* Asignate new value to DLL */
 
-	if (*head == NULL)
+	if (*head == NULL) /* Validate if the second pointer is null */
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	new_node->next = *head;
-	(*head)->prev = new_node;
-	*head = new_node;
+	new_node->next = *head; /* Jump pointer to pointer */
+	(*head)->prev = new_node; /* The second node validate  */
+	*head = new_node; /* The firts pointer take the new node */
 
 	return (new_node);
 }
