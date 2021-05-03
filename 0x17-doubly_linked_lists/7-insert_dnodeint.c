64eedb3ade15d1d109a777e058ce5cfe5fa63bb9
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-02 20:37:03 -0500

    (Fix) edge cases first values

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,19 +13,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-
+	if (idx > 0 && *h == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n; /* Assing the value to new node created */
+
+	new_node->next = NULL; /* Assing NULl to declarate the of DLL*/
+	new_node->prev = NULL; /* Assing NULl to declarate the of DLL*/
+
+	if (*h == NULL) /* Validate if the second pointer is NUll  */
+	{
+		(*h) = new_node;
+		return (new_node); /* return the node end to assig pocsition  */
+	}
 	current_node = *h; /* Assing value of header */
 
-	/* scroll to the appropriate index */
-	for (idx = idx; idx > 1; idx--)
+	for (idx = idx; idx > 1; idx--) /* scroll to the appropriate index */
 		current_node = current_node->next;
-
-	new_node->n = n; /* get value the node  */
 
 	/* Add the next value to ->next of new node */
 	new_node->next = current_node->next;
