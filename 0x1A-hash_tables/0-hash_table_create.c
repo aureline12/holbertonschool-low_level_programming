4334502b2f91261e075c2b9f34feef9d553b5f0f
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-28 15:32:35 -0500

    (Add) initial files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - fuction creates an hash table
+ *
+ * @size: size of the hash table
+ * Return: table whit new size
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+
+	/* Assign the double pointer array */
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+		return (NULL);
+
+	return (table);
+}
