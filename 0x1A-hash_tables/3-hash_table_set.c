b0acd61049d8ee1c45b4844c1140f4bc9f1ded3c
Sergioarg <sergio_ramos53@yahoo.com>
2021-05-28 23:54:25 -0500

    (Add) 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table.
+ *
+ * @ht: hash table to add values
+ * @key: add documentation
+ * @value: add documentation
+ * Return: int
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *item;
+	unsigned long int index;
+
+	/* Get the index segun la key the key */
+	index = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[index];
+
+	while (item != NULL)
+	{
+		if (strcmp(item->key, key) == 0)
+		{
+			free(item->value);
+			item->value = strdup(value);
+			if (item->value == NULL)
+				return (0);
+			return (1);
+		}
+		item = item->next;
+	}
+
+	item = malloc(sizeof(hash_node_t));
+
+	if (item == NULL)
+		return (0);
+
+	item->key = strdup(key);
+	if (item->key == NULL)
+	{
+		free(item);
+		return (0);
+	}
+	item->value = strdup(value);
+	if (item->value == NULL)
+	{
+		free(item->key);
+		free(item);
+		return (0);
+	}
+
+	return (1);
+}
