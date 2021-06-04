7ff5b7d84ffd8c35771e00ff1377b698fece5280
Sergio Ramos <51498566+Sergioarg@users.noreply.github.com>
2021-06-04 17:36:52 -0500

    (Fix) Betty Style

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - function that prints a hash table 
- * @ht: hash table to print 
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table to print
  */
 void hash_table_print(const hash_table_t *ht)
 {
