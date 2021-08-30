5fbf38c96525f9a54363ba8bc44c7883c4124957
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-30 15:38:12 -0500

    :tada: Add initial files

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers
+ *
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: int (-1)
+ */
+int binary_search(int *array, size_t size, int value)
+{
+
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Linear search algorithm */
+int linear_search(int *array, size_t size, int value);
+
+/* Binary search algorithm */
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
