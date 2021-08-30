db70262d432bec6b9c2520c4f7b1b0897c526f99
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-30 15:55:23 -0500

    Add solution for file 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,5 +9,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
 
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (array[i] == value)
+			return (i);
+
+	return (-1);
 }
